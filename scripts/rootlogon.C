d5fca7aa569346967a3aab314ba5f14fbb8c78bf
Javier Rico <jrico@ifae.es>
2018-12-05 17:40:35 +0100

    Look for gLike library always in correct place

--- scripts/rootlogon.C
+++ scripts/rootlogon.C
@@ -42,7 +42,7 @@ void rootlogon()
 
     if (!load(libdir,libfilename)) return;
 
-    gInterpreter->AddIncludePath(libdir+"src");
+    gInterpreter->AddIncludePath(gLikeDir+"include");
     gStyle->SetCanvasColor(0);
     gStyle->SetCanvasBorderMode(0);
     gStyle->SetFrameBorderMode(0);
