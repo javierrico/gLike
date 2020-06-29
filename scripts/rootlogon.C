5ac55124b7f66699f210570beb14580d10dc6b84
cosimoNigro <cosimonigro2@gmail.com>
2020-06-29 11:37:58 +0200

    figured how to call the rootlogon.C globally

--- scripts/rootlogon.C
+++ scripts/rootlogon.C
@@ -13,9 +13,9 @@ void rootlogon()
     cout << "\033[34m\033[1m" << "Searching gLike in " << gLikeBuildDir << "/lib" << "\033[0m" << endl ;
     TString libFileNameUnix = "libgLike.so";
     TString libFileNameMacOs = "libgLike.dylib";
-    // search if unix library exists, empty string if not
+    // search if unix library exists, empty string if it does not
     TString libUnixPath =  gSystem->FindFile(gLikeBuildDir + "/lib/", libFileNameUnix);
-    // search if MacOs library exists, empty string if not
+    // search if MacOs library exists, empty string if it does not
     TString libMacOsPath =  gSystem->FindFile(gLikeBuildDir + "/lib/", libFileNameMacOs);
     
     if (!libUnixPath.IsNull()) {
@@ -27,7 +27,7 @@ void rootlogon()
         gSystem->Load(libMacOsPath);
         cout << "\033[33m\033[1m" << "done." << endl;  
     } else {
-        cout << "\033[33m\033[1m" << "Error: gLike library not found" << endl;
+        cout << "\033[33m\033[1m" << "Error: no gLike library found." << endl;
         return;
     }
     
