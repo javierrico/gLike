951483a9eb58dc95a69d8dd096a09c9e1ddd3f48
Daniel Kerszberg <dkerszberg@ifae.es>
2020-09-16 09:25:27 +0200

    More cleaning

--- include/LineSearchLkl.h
+++ include/LineSearchLkl.h
@@ -20,9 +20,9 @@ class LineSearchLkl : public Iact1dUnbinnedLkl
   virtual ~LineSearchLkl();
 
   inline  TH1F*    GetHdNdEpBkg()             const {return fHdNdEpBkg;}
-  inline  TH1F*    GetHdNdEpSignal()          const {return fHdNdEpSignal;}
+  //inline  TH1F*    GetHdNdEpSignal()          const {return fHdNdEpSignal;}
 
-  inline  Double_t GetdNdEpSignalIntegral()   {CheckHistograms(kFALSE); if(!fHdNdEpSignal) return 0; return fHdNdEpSignal->GetBinContent(0);}
+  //inline  Double_t GetdNdEpSignalIntegral()   {CheckHistograms(kFALSE); if(!fHdNdEpSignal) return 0; return fHdNdEpSignal->GetBinContent(0);}
   Int_t ReaddNdEpSignal(TString filename);
   virtual TH1F* GetHdNdEpModelBkg(Bool_t isDifferential=kTRUE,Int_t nbins=0) const;
   virtual TH1F* GetHdNdEpOn(Bool_t isDifferential=kTRUE,Int_t nbins=0)  const; 
@@ -39,13 +39,13 @@ class LineSearchLkl : public Iact1dUnbinnedLkl
   Int_t ComputeBkgModelFromOnHisto();
 
   Int_t CheckHistograms(Bool_t checkdNdEpBkg=kTRUE);
-  Int_t ResetdNdESignal();
+  /*Int_t ResetdNdESignal();
   Int_t SetdNdESignalFunction(TString function,Float_t p0=0,Float_t p1=0,Float_t p2=0,Float_t p3=0,Float_t p4=0,Float_t p5=0,Float_t p6=0,Float_t p7=0,Float_t p8=0,Float_t p9=0);
-  Int_t SetdNdESignalFunction(TF1* function,Float_t emin=0,Float_t emax=1e9);
+  Int_t SetdNdESignalFunction(TF1* function,Float_t emin=0,Float_t emax=1e9);*/
 
  private:
   TH1F*    fHdNdEpBkg;              //-> dN/dE'dt vs E' for background events (normalized)
-  TH1F*    fHdNdEpSignal;           //-> dN/dE' vs E' histogram for signal events (normalized)
+  //TH1F*    fHdNdEpSignal;           //-> dN/dE' vs E' histogram for signal events (normalized)
 
   ClassDef(LineSearchLkl,1) // Line Search Likelihood
 };
